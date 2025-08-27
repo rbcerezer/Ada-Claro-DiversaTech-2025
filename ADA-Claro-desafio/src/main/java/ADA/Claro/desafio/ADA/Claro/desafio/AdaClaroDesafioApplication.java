83d672a13cd334b716ef3883b663909fa385a89d
Gabriel Lorran <glorran0810@gmail.com>
2025-08-27 20:20:50 -0300

    UP spring boot

---
+++ ADA-Claro-desafio/src/main/java/ADA/Claro/desafio/ADA/Claro/desafio/AdaClaroDesafioApplication.java
@@ -0,0 +1,13 @@
+package ADA.Claro.desafio.ADA.Claro.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaClaroDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaClaroDesafioApplication.class, args);
+	}
+
+}
---
+++ ADA-Claro-desafio/src/test/java/ADA/Claro/desafio/ADA/Claro/desafio/AdaClaroDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package ADA.Claro.desafio.ADA.Claro.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaClaroDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
